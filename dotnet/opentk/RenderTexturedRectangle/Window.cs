acd57ac76c513bd9109113ba4c4de3a9b1a088bc
lawrence-laz <laurynas@lazauskas.info>
2022-12-17 21:02:31 +0200

    Add OpenTK rendering textures

---
+++ dotnet/opentk/RenderTexturedRectangle/Window.cs
@@ -0,0 +1,115 @@
+namespace PlaygroundOpenTK;
+
+public class Window : GameWindow
+{
+    // Because we're adding a texture, we modify the vertex array to include texture coordinates.
+    // Texture coordinates range from 0.0 to 1.0, with (0.0, 0.0) representing the bottom left, and (1.0, 1.0) representing the top right.
+    // The new layout is three floats to create a vertex, then two floats to create the coordinates.
+    private readonly float[] _vertices =
+    {
+        // Position         Texture coordinates
+         0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+    };
+
+    private readonly uint[] _indices =
+    {
+        0, 1, 3, // The first triangle will be the top-right half of the triangle
+        1, 2, 3  // Then the second will be the bottom-left half of the triangle
+    };
+
+    private int _vertexBufferObject;
+
+    private int _vertexArrayObject;
+
+    private Shader _shader;
+
+    private int _elementBufferObject;
+    // For documentation on this, check Texture.cs.
+    private Texture _texture;
+
+    public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+        : base(gameWindowSettings, nativeWindowSettings)
+    {
+    }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        _vertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+
+        _elementBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+        _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+        _shader.Use();
+
+        // Because there's now 5 floats between the start of the first vertex and the start of the second,
+        // we modify the stride from 3 * sizeof(float) to 5 * sizeof(float).
+        // This will now pass the new vertex array to the buffer.
+        var vertexLocation = _shader.GetAttribLocation("aPosition");
+        GL.EnableVertexAttribArray(vertexLocation);
+        GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+
+        // Next, we also setup texture coordinates. It works in much the same way.
+        // We add an offset of 3, since the texture coordinates comes after the position data.
+        // We also change the amount of data to 2 because there's only 2 floats for texture coordinates.
+        var texCoordLocation = _shader.GetAttribLocation("aTexCoord");
+        GL.EnableVertexAttribArray(texCoordLocation);
+        GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+
+        _texture = Texture.LoadFromFile("Resources/sprite.png");
+        _texture.Use(TextureUnit.Texture0);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+
+        GL.Enable(EnableCap.Blend);
+        GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        GL.BindVertexArray(_vertexArrayObject);
+
+
+        _texture.Use(TextureUnit.Texture0);
+        _shader.Use();
+
+        GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+        SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        var input = KeyboardState;
+
+        if (input.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+
+        GL.Viewport(0, 0, Size.X, Size.Y);
+    }
+
+}
