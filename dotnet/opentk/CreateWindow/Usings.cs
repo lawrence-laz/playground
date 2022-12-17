0ad51239fc4c0d05870c66d03e51257b0ffd2f04
lawrence-laz <laurynas@lazauskas.info>
2022-12-17 13:17:28 +0200

    Add OpenTK window creating example

---
+++ dotnet/opentk/CreateWindow/Usings.cs
@@ -0,0 +1,5 @@
+global using OpenTK.Mathematics;
+global using OpenTK.Windowing.Common;
+global using OpenTK.Windowing.Desktop;
+global using OpenTK.Windowing.GraphicsLibraryFramework;
+global using CreateWindow;
