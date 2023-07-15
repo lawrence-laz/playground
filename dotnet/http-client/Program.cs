2ee5e00e736bd6a1b74ed29e07cea44e000fc129
lawrence-laz <laurynas@lazauskas.info>
2023-07-16 02:23:41 +0300

    Add simple HttpClient example

---
+++ dotnet/http-client/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+var client = new HttpClient();
+var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, "https://www.postman-echo.com/get"));
+var content = await response.Content.ReadAsStringAsync();
+var responseObject = JsonSerializer.Deserialize<PostmanResponse>(content);
+System.Console.WriteLine(content);
+System.Console.WriteLine($"URL: {responseObject.url}, host: {responseObject.headers.host}");
+
+public record PostmanResponseHeaders(string host);
+public record PostmanResponse(string url, PostmanResponseHeaders headers);
