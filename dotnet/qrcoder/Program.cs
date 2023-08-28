1ccd9f9d017b6a2ad6789b52fd683f7235688ae3
Laurynas Lazauskas <llaz@llaz-macbook.local>
2023-08-28 21:29:32 +0300

    Add QR code generating example

---
+++ dotnet/qrcoder/Program.cs
@@ -0,0 +1,10 @@
+﻿using QRCoder;
+using System.IO;
+
+QRCodeGenerator qrGenerator = new QRCodeGenerator();
+QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://www.google.com/", QRCodeGenerator.ECCLevel.Q);
+PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+byte[] qrCodeAsPngByteArr = qrCode.GetGraphic(20);
+
+
+File.WriteAllBytes("qr.png", qrCodeAsPngByteArr);
