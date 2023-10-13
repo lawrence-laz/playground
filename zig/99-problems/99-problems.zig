9991a94cbe0f97ce3bef6ab4b1f183bcc6bd63c6
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2023-10-13 23:45:09 +0300

    Add zig #2 from 99 problems

--- zig/99-problems/99-problems.zig
+++ zig/99-problems/99-problems.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
+// #1 Write a function that returns the last element of a list.
 fn last(list: std.ArrayList(u8)) u8 {
     return list.items[list.items.len - 1];
 }
 
-// #1
 test last {
     var list = std.ArrayList(u8).init(std.testing.allocator);
     defer list.deinit();
@@ -13,4 +13,33 @@ test last {
     try list.append('c');
     try list.append('d');
     try std.testing.expect(last(list) == 'd');
+}
+
+// #2 Find the last but one (last and penultimate) elements of a list.
+fn lastTwo(list: std.ArrayList(u8)) ?struct { next_to_last: u8, last: u8 } {
+    return if (list.items.len >= 2)
+        .{ .next_to_last = list.items[list.items.len - 2], .last = list.items[list.items.len - 1] }
+    else
+        null;
+}
+
+test lastTwo {
+    {
+        // List with many items
+        var list = std.ArrayList(u8).init(std.testing.allocator);
+        defer list.deinit();
+        try list.append('a');
+        try list.append('b');
+        try list.append('c');
+        try list.append('d');
+        try std.testing.expect(lastTwo(list).?.next_to_last == 'c');
+        try std.testing.expect(lastTwo(list).?.last == 'd');
+    }
+    {
+        // List with a single item
+        var list = std.ArrayList(u8).init(std.testing.allocator);
+        defer list.deinit();
+        try list.append('a');
+        try std.testing.expect(lastTwo(list) == null);
+    }
 }
