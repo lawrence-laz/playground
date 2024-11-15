1f16c35ca36ce005acb18ff83628a0cb9e01d596
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-11-15 23:40:32 +0200

    Sync request in dotnet (don't ask why)

---
+++ dotnet/sync-http-request/Program.cs
@@ -0,0 +1,16 @@
+﻿// Need to make a synchronous request
+#pragma warning disable SYSLIB0014
+
+var request = System.Net.WebRequest.Create("https://postman-echo.com/get?message=hello-world");
+var response = (System.Net.HttpWebResponse)request.GetResponse();
+if (response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    using var responseStream = response.GetResponseStream();
+    using var reader = new System.IO.StreamReader(responseStream);
+    var responseFromServer = reader.ReadToEnd();
+    Console.WriteLine(responseFromServer);
+}
+else
+{
+    Console.WriteLine("Error: " + response.StatusCode);
+}
